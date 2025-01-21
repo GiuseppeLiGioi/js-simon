47f47d8e3099f26402a41a6fa6aaa1d02146372d
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-01-21 14:26:13 +0100

    ADD: Number-list and countdown

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
 **Descrizione:**
-Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
-Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
+ Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono Visualizzare in pagina 5 numeri casuali.e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 **NOTA**: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
 **BONUS:**
@@ -15,10 +15,21 @@ Consigli del giorno:
 > - Immaginate la logica come fosse uno snack: "Dati 2 array di numeri, indica quali e quanti numeri ci sono in comune tra i due array"
 */
 
-
+let Numeri = [];
+let numeriHtml = document.getElementById('numbers-list');
 let divHtml = document.getElementById('countdown');
-
 let counter = 30;
+
+
+
+for(let i=0; i<5; i++){
+    let numeroCasuale = Math.floor(Math.random() * (50 - 1)) + 1;
+    Numeri.push(numeroCasuale);
+    numeriHtml.innerHTML = Numeri;
+    
+
+}
+
 
 const ContoAllaRovescia = setInterval(function(){
 counter--;
