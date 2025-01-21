299274e51314c2667f5b3b32f502d90d2f8a6def
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-01-21 14:35:53 +0100

    ADD: timeout, to make the numbers disappear

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
 **Descrizione:**
- Da lì parte un timer di 30 secondi.
-Dopo 30 secondi i numeri scompaiono Visualizzare in pagina 5 numeri casuali.e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
+ Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 **NOTA**: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
 **BONUS:**
@@ -18,7 +18,7 @@ Consigli del giorno:
 let Numeri = [];
 let numeriHtml = document.getElementById('numbers-list');
 let divHtml = document.getElementById('countdown');
-let counter = 30;
+let counter = 5;
 
 
 
@@ -35,8 +35,17 @@ const ContoAllaRovescia = setInterval(function(){
 counter--;
 if(counter === 0){
     clearInterval(ContoAllaRovescia);
-    divHtml.innerHTML="Hai finito il tempo a disposizione per memorizzare i numeri";
+    divHtml.innerHTML="Hai finito il tempo per memorizzare i numeri";
 }else{
     divHtml.innerHTML=counter;
 }
 }, 1000);
+
+
+setTimeout(function(){
+Numeri = [];
+numeriHtml.innerHTML = Numeri;
+}, 5000);
+
+
+
